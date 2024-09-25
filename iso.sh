07cb76755f3bea55cfb1848d83cff79aef0cc13d
Potato Lover <118972777+winvistalover@users.noreply.github.com>
2024-09-25 05:22:38 -0500

    Update iso.sh

--- iso.sh
+++ iso.sh
@@ -8,7 +8,14 @@ mkdir -p isodir/boot/grub
 
 cp sysroot/boot/potatoos.kernel isodir/boot/potatoos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "potatoOS" {
+GRUB_DEFAULT=pos
+GRUB_TIMEOUT=5
+GRUB_GFXMODE=1440x900
+color_normal=cyan/dark-gray
+color_highlight=light-magenta/black
+
+
+menuentry "PotatoOS"  --id pos{
 	multiboot /boot/potatoos.kernel
 }
 EOF
